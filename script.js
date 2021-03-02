980258c761aaf4e871f48f891667938873f14290
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-03-02 08:44:38 -0300

    atualizando click

---
+++ script.js
@@ -0,0 +1,12 @@
+function clicarMenu() {
+    const submenu = document.getElementById("menuAbrir");
+    if(submenu.style.display == 'none'){
+        submenu.style.display = 'block';
+      }else{
+        submenu.style.display = 'none';
+      }
+      
+
+
+
+}

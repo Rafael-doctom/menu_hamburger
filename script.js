2165c992ca69c2e2fab4a48a3720579f2fad374f
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-04-08 14:05:01 -0300

    alterando

---
+++ script.js
@@ -0,0 +1,7 @@
+const $hamburger = document.querySelector('.hamburger')
+
+const $menuOpen = document.querySelector('.menuOpen')
+
+$menuOpen.addEventListener('click', function(){
+    $menu.classList.add('menuAbrir')
+})

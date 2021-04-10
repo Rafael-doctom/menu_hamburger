339987995ef55e669b113d323a0feb562c43f2c1
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-04-10 14:06:21 -0300

    adicionando novos codigos

--- script.js
+++
@@ -1,7 +0,0 @@
-const $hamburger = document.querySelector('.hamburger')
-
-const $menuOpen = document.querySelector('.menuOpen')
-
-$menuOpen.addEventListener('click', function(){
-    $menu.classList.add('menuAbrir')
-})

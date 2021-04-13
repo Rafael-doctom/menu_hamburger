6699567583325d4c7ebc0c61b0f2761f1a54b864
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-04-13 14:16:53 -0300

    quase

---
+++ script.js
@@ -0,0 +1,14 @@
+function clicarMenu(){
+    // const clicarMenu = document.getElementById("botao")
+
+    const menuLista = document.getElementById("navMenu")
+
+
+    if(menuLista.style.display=== 'none'){
+        menuLista.style.display = 'flex'
+    }else{
+        navMenu.style.display = 'flex';
+      }
+
+
+}

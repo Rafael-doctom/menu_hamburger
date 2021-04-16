ac9cabbcc5d081566444e0254a1f4bcf72362e85
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-04-16 13:47:40 -0300

    atualizando

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 function clicarMenu(event){
- 
-
     const menuLista = document.getElementById("navMenu")
-
-
     if(menuLista.style.display=== 'none'){
         menuLista.style.display = 'block'
     }else{
-        menuLista.style.display = 'block';
+        menuLista.style.display = 'none';
     }
-
     event.preventDefault();
+
 }

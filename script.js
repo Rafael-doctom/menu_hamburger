ec7312c998b6e2364c175ef22614727aa6b77092
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-04-19 14:20:23 -0300

    ok

---
+++ script.js
@@ -0,0 +1,28 @@
+// abrir função
+function menuClicar(event){
+    // constante para o menu hamburger
+    const menuClicar = document.querySelector(".menuHamburger")
+
+    //  constante para o menu LISTA
+    const menu = document.querySelector(".menu")
+
+
+    //  condições if else
+
+    if(menu.style.display === 'none'){
+        menu.style.display = ' block'
+    }else{
+        menu.style.display = 'block'
+    }
+
+
+    event.preventdefault()
+
+    
+
+
+
+
+
+
+}

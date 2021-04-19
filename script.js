dcaff583ac0ce7d0ae068f70bc10939b6e030895
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-04-19 14:23:41 -0300

    Ok

--- script.js
+++ script.js
@@ -6,17 +6,16 @@ function menuClicar(event){
     //  constante para o menu LISTA
     const menu = document.querySelector(".menu")
 
-
     //  condições if else
 
     if(menu.style.display === 'none'){
-        menu.style.display = ' block'
-    }else{
         menu.style.display = 'block'
+    }else{
+        menu.style.display = 'none'
     }
 
 
-    event.preventdefault()
+    event.preventDefault()
 
     
 
